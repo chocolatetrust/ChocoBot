3bea0fb64acd28e95c962db8c0b98a2d7d9cbddb
Cassidy Dingenskirchen <admin@15318.de>
2020-12-05 17:18:24 +0100

    fixup

--- default.nix
+++ default.nix
@@ -1,8 +1,8 @@
 with import (builtins.fetchTarball {
-  # nixos-unstable on 2020-07-18
+  # nixos-unstable on 2020-10-13
   url =
-    "https://github.com/NixOS/nixpkgs/tarball/d7e20ee25ed8aa1f0f24a9ca77026c6ef217f6ba";
-  sha256 = "1ar7prnrmmlqj17g57nqp82hgy5283dxb94akaqrwpbaz7qfwi4y";
+    "https://github.com/NixOS/nixpkgs/tarball/420f89ceb267b461eed5d025b6c3c0e57703cc5c";
+  sha256 = "0c9kr76p6nmf4z2j2afgcddckbaxq6kxlmp1895h6qamm1c0ypb9";
 }) { };
 
 poetry2nix.mkPoetryApplication { projectDir = ./.; }
